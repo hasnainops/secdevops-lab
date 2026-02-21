610e2ae45887cb3100a14a8d0a1b7b5f6caab491
Hasnain <hasnainops@users.noreply.github.com>
2026-02-21 13:08:42 +0000

    Fresh start: Node app + tests

--- index.js
+++ index.js
@@ -5,6 +5,4 @@ const server = http.createServer((req, res) => {
   res.end("Hello, SecDevOps!\n");
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+server.listen(3000, () => console.log("Server is running on port 3000"));
